5040cfcd954a25eac83d3973f016b89c8ff4895d
Steven Brooks <sgb@Stevens-MacBook-Pro.local>
2017-11-12 01:50:49 -0500

    DRYing it Up

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -20,14 +20,11 @@ public class TextAnalysis {
 		stopListWords = stopList.getWordsArray();
 	}
 	/**
-	 * this method delivers the frequency of each letter
-	 * and returns the top 10 letters in frequency
+	 * this method computes the frequency of each letter
+	 * and prints the top 10 letters by frequency
 	 */
 	public void rankTopLetters(int n) {
 		HashMap<String,Integer> lettersCount = new HashMap<>();
-		//TODO take in the full letter array from a book
-		//and calculate the freq, and then print out 
-		//top ten
 		String[] alphabet = getAlphabet();
 		for (int i = 0; i < alphabet.length; i++) {
 			for (int j = 0; j < letters.size(); j++) {
@@ -42,71 +39,25 @@ public class TextAnalysis {
 				}
 			}
 		}
-
-		Integer largestValue = 0;
-		Integer lastValue = null;
-		String largestKey = null;
-		String lastKey = null;
-		
-		for (int i = 0; i < n; i++) {
-			for (String key : lettersCount.keySet()) {
-				lastValue = lettersCount.get(key);
-				lastKey = key;
-				if (lastValue > largestValue) {
-					largestValue = lastValue;
-					largestKey = lastKey;
-				}
-			}
-			System.out.println(largestKey + " : " + largestValue);
-			lettersCount.remove(largestKey);
-			largestValue = 0;
-			lastValue = 0;
-		}
+		printTopNbyFreq(lettersCount, n);
 	}
 	/**
-	 * this method delivers the frequency of each letter
-	 * and returns the top 10 letters in frequency
+	 * this method computes the frequency of each word
+	 * and prints the top 10 words by frequency.
 	 */
 	public void rankTopWords(int n) {
 		HashMap<String,Integer> wordsCount = new HashMap<>();
-		//TODO take in the full words array from a book
-		//and calculate the freq, and then print out 
-		//top ten
-		for (int i = 0; i < words.size(); i++) {
-			if (wordsCount.containsKey(words.get(i).getWord())) {
-				wordsCount.put(words.get(i).getWord(), 
-						wordsCount.get(words.get(i).getWord()) + 1);
-			} else {
-				wordsCount.put(words.get(i).getWord(), 1);
-			}
-		}
-		Integer largestValue = 0;
-		Integer lastValue = null;
-		String largestKey = null;
-		String lastKey = null;
-		
-		for (int i = 0; i < n; i++) {
-			for (String key : wordsCount.keySet()) {
-				lastValue = wordsCount.get(key);
-				lastKey = key;
-				if (lastValue > largestValue) {
-					largestValue = lastValue;
-					largestKey = lastKey;
-				}
-			}
-			System.out.println(largestKey + " : " + largestValue);
-			wordsCount.remove(largestKey);
-			largestValue = 0;
-			lastValue = 0;
-		}
+		wordsCount = calculateFreqFromWords(words);
+		printTopNbyFreq(wordsCount, n);
 	}
 	/**
-	 * this method delivers the frequency of each letter
-	 * and returns the top 10 letters in frequency
+	 * this method computes the frequency of each word,
+	 * compares it against a stop-list, and prints out 
+	 * the top 10 letters by frequency which aren't in 
+	 * the stop-list.
 	 */
 	public void rankTopWordsWithStopList(int n) {
 		HashMap<String,Integer> wordsCount = new HashMap<>();
-		//TODO take out the stop list words from the book words list
 		for (int i = 0; i < stopListWords.size(); i++) {
 			for (int j = 0; j < words.size(); j++) {
 				if (words.get(j).getWord().equals(stopListWords.get(i).getWord())) {
@@ -114,22 +65,43 @@ public class TextAnalysis {
 				}
 			}
 		}
-		for (int i = 0; i < words.size(); i++) {
-			if (wordsCount.containsKey(words.get(i).getWord())) {
-				wordsCount.put(words.get(i).getWord(), 
-						wordsCount.get(words.get(i).getWord()) + 1);
+		wordsCount = calculateFreqFromWords(words);
+		printTopNbyFreq(wordsCount, n);
+	}
+	/**
+	 * This method creates a HashMap where the key are words
+	 * and the values are how many times that word appears in the
+	 * provided ArrayList of type Word.
+	 * @param stringArray an ArrayList of type Word
+	 * @return a HashMap with frequencies
+	 */
+	private HashMap<String,Integer> calculateFreqFromWords(ArrayList<Word> stringArray) {
+		HashMap<String,Integer> wordsCount = new HashMap<>();
+		for (int i = 0; i < stringArray.size(); i++) {
+			if (wordsCount.containsKey(stringArray.get(i).getWord())) {
+				wordsCount.put(stringArray.get(i).getWord(), 
+						wordsCount.get(stringArray.get(i).getWord()) + 1);
 			} else {
-				wordsCount.put(words.get(i).getWord(), 1);
+				wordsCount.put(stringArray.get(i).getWord(), 1);
 			}
 		}
+		return wordsCount;
+	}
+	/**
+	 * This method prints the n highest frequency words/letters 
+	 * in a HashMap
+	 * @param hashmap a HashMap of words/letters and their frequencies
+	 * @param numberOfRanks the desired number of ranks
+	 */
+	private void printTopNbyFreq(HashMap<String,Integer> hashmap, int numberOfRanks) {
 		Integer largestValue = 0;
 		Integer lastValue = null;
 		String largestKey = null;
 		String lastKey = null;
-		
-		for (int i = 0; i < n; i++) {
-			for (String key : wordsCount.keySet()) {
-				lastValue = wordsCount.get(key);
+
+		for (int i = 0; i < numberOfRanks; i++) {
+			for (String key : hashmap.keySet()) {
+				lastValue = hashmap.get(key);
 				lastKey = key;
 				if (lastValue > largestValue) {
 					largestValue = lastValue;
@@ -137,80 +109,14 @@ public class TextAnalysis {
 				}
 			}
 			System.out.println(largestKey + " : " + largestValue);
-			wordsCount.remove(largestKey);
+			hashmap.remove(largestKey);
 			largestValue = 0;
 			lastValue = 0;
 		}
 	}
 	/**
-	 * This method counts the freq for each letter
-	 * in the alphabet in a book
-	 * @return a hashmap of letters mapped to freq
-	 */
-	public HashMap<String,Integer> letterCount() {
-		HashMap<String,Integer> topTen = new HashMap<>();
-		//TODO take in the full letter array from alice
-		//and calculate the freq, and then filter for 
-		//top ten
-		String[] alphabet = getAlphabet();
-		for (int i = 0; i < alphabet.length; i++) {
-			for (int j = 0; j < letters.size(); j++) {
-				if (letters.get(j).getLetter().toString()
-						.equalsIgnoreCase(alphabet[i])) {
-					if (topTen.containsKey(alphabet[i])) {
-						topTen.put(alphabet[i], 
-								topTen.get(alphabet[i]) + 1);
-					} else {
-						topTen.put(alphabet[i], 1);
-					}
-				}
-			}
-		}
-		return topTen;
-	}
-	/**
-	 * This method ranks a HashMap based on its largest
-	 * values, and returns an ArrayList of keys that correspond
-	 * to the top n values in that HashMap
-	 * @param toRank the HashMap of type <String,Integer> to rank
-	 * @param n the top n values for the HashMap
-	 * @return an ArrayList of type String of the top n keys of the HashMap
-	 */
-	public ArrayList<String> rankHashMap(HashMap<String,Integer> toRank, int n) {
-		ArrayList<String> ranked = new ArrayList<>();
-		String nextKey = null;
-		for (int i = 0; i < n; i++) {
-			nextKey = getLargestValue(toRank);
-			ranked.add(nextKey);
-		}
-		return ranked;
-	}
-	/**
-	 * This method gets the largest value's key in a hashmap
-	 * and then removes the key and the value
-	 * @param getMax the hashmap to be analyzed
-	 * @return the key of the largest value
-	 */
-	private String getLargestValue(HashMap<String, Integer> getMax) {
-		Integer largestValue = 0;
-		Integer lastValue = null;
-		String largestKey = null;
-		String lastKey = null;
-		
-		for (String key : getMax.keySet()) {
-			lastValue = getMax.get(key);
-			lastKey = key;
-			if (lastValue > largestValue) {
-				largestValue = lastValue;
-				largestKey = lastKey;
-			}
-		}
-		getMax.remove(largestKey);
-		return largestKey;
-	}
-	/**
 	 * This method returns a string array 
-	 * of the letters in the english alphabet in caps
+	 * of the letters in the English alphabet in caps
 	 * @return string array of the 26 letters 
 	 */
 	private String[] getAlphabet() {
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -4,22 +4,28 @@ import java.util.HashMap;
 public class UserInterface {
 
 	public static void main(String[] args) {
+		
+		String[] bookList = {"alice-in-wonderland.txt",
+				"christmas-carol.txt", "huck-finn.txt",
+				"les-mis.txt", "metamorphosis.txt",
+				"my-man-jeeves.txt", "pride-prejudice.txt",
+				"tale-of-two-cities.txt", "tom-sawyer.txt"};
 
-		BookReader book = new BookReader("alice-in-wonderland.txt");
+//		for (String element : bookList) {
+		BookReader book = new BookReader(bookList[8]);
 		ArrayList<Letter> bookLetters = book.getLettersArray();
 		ArrayList<Word> bookWords = book.getWordsArray();
 		TextAnalysis textAnalysis = new TextAnalysis(bookLetters, bookWords);
 		
+//		System.out.println(element);
+		System.out.println("===============================");
 		textAnalysis.rankTopLetters(10);
-		System.out.println("===================");
+		System.out.println("===============================");
 		textAnalysis.rankTopWords(10);
-		System.out.println("===================");
+		System.out.println("===============================");
 		textAnalysis.rankTopWordsWithStopList(10);
-//		for (int i = 0; i < bookWords.size(); i++) {
-//			System.out.println(bookWords.get(i).getWord());
+		System.out.println("===============================");
 //		}
-//		System.out.println(bookWords.size());
-		
 		
 	}
 }
